d3e05382c585321032f6613da51fe4d612a0b5ec
Honeyful <rubble-ice@outlook.com>
2017-03-18 19:17:22 +0900

    init Commit

---
+++ PathEditor/MainForm.Designer.vb
@@ -0,0 +1,244 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form은 Dispose를 재정의하여 구성 요소 목록을 정리합니다.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows Form 디자이너에 필요합니다.
+    Private components As System.ComponentModel.IContainer
+
+    '참고: 다음 프로시저는 Windows Form 디자이너에 필요합니다.
+    '수정하려면 Windows Form 디자이너를 사용하십시오.  
+    '코드 편집기에서는 수정하지 마세요.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.fbd = New System.Windows.Forms.FolderBrowserDialog()
+        Me.lb = New System.Windows.Forms.ListBox()
+        Me.txtPath = New System.Windows.Forms.TextBox()
+        Me.btnBrowse = New System.Windows.Forms.Button()
+        Me.btnExport = New System.Windows.Forms.Button()
+        Me.btnUpdate = New System.Windows.Forms.Button()
+        Me.btnDelete = New System.Windows.Forms.Button()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.btnOK = New System.Windows.Forms.Button()
+        Me.btnCancel = New System.Windows.Forms.Button()
+        Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
+        Me.Panel3 = New System.Windows.Forms.Panel()
+        Me.radProcess = New System.Windows.Forms.RadioButton()
+        Me.radSystem = New System.Windows.Forms.RadioButton()
+        Me.radUser = New System.Windows.Forms.RadioButton()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.TableLayoutPanel1.SuspendLayout()
+        Me.Panel3.SuspendLayout()
+        Me.Panel2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'lb
+        '
+        Me.lb.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.lb.FormattingEnabled = True
+        Me.lb.ItemHeight = 12
+        Me.lb.Location = New System.Drawing.Point(0, 0)
+        Me.lb.Name = "lb"
+        Me.lb.Size = New System.Drawing.Size(1017, 423)
+        Me.lb.TabIndex = 3
+        '
+        'txtPath
+        '
+        Me.txtPath.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.txtPath.Location = New System.Drawing.Point(13, 14)
+        Me.txtPath.Name = "txtPath"
+        Me.txtPath.Size = New System.Drawing.Size(898, 21)
+        Me.txtPath.TabIndex = 4
+        '
+        'btnBrowse
+        '
+        Me.btnBrowse.Anchor = System.Windows.Forms.AnchorStyles.Right
+        Me.btnBrowse.Location = New System.Drawing.Point(918, 13)
+        Me.btnBrowse.Name = "btnBrowse"
+        Me.btnBrowse.Size = New System.Drawing.Size(75, 23)
+        Me.btnBrowse.TabIndex = 5
+        Me.btnBrowse.Text = "..."
+        Me.btnBrowse.UseVisualStyleBackColor = True
+        '
+        'btnExport
+        '
+        Me.btnExport.Location = New System.Drawing.Point(12, 39)
+        Me.btnExport.Name = "btnExport"
+        Me.btnExport.Size = New System.Drawing.Size(75, 29)
+        Me.btnExport.TabIndex = 6
+        Me.btnExport.Text = "Export"
+        Me.btnExport.UseVisualStyleBackColor = True
+        '
+        'btnUpdate
+        '
+        Me.btnUpdate.Anchor = System.Windows.Forms.AnchorStyles.Right
+        Me.btnUpdate.Location = New System.Drawing.Point(756, 38)
+        Me.btnUpdate.Name = "btnUpdate"
+        Me.btnUpdate.Size = New System.Drawing.Size(75, 29)
+        Me.btnUpdate.TabIndex = 7
+        Me.btnUpdate.Text = "Update"
+        Me.btnUpdate.UseVisualStyleBackColor = True
+        '
+        'btnDelete
+        '
+        Me.btnDelete.Anchor = System.Windows.Forms.AnchorStyles.Right
+        Me.btnDelete.Location = New System.Drawing.Point(837, 38)
+        Me.btnDelete.Name = "btnDelete"
+        Me.btnDelete.Size = New System.Drawing.Size(75, 29)
+        Me.btnDelete.TabIndex = 8
+        Me.btnDelete.Text = "Delete"
+        Me.btnDelete.UseVisualStyleBackColor = True
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Anchor = System.Windows.Forms.AnchorStyles.Right
+        Me.btnAdd.Location = New System.Drawing.Point(918, 38)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(75, 29)
+        Me.btnAdd.TabIndex = 9
+        Me.btnAdd.Text = "Add"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'btnOK
+        '
+        Me.btnOK.Anchor = System.Windows.Forms.AnchorStyles.Right
+        Me.btnOK.Location = New System.Drawing.Point(837, 73)
+        Me.btnOK.Name = "btnOK"
+        Me.btnOK.Size = New System.Drawing.Size(75, 29)
+        Me.btnOK.TabIndex = 10
+        Me.btnOK.Text = "OK"
+        Me.btnOK.UseVisualStyleBackColor = True
+        '
+        'btnCancel
+        '
+        Me.btnCancel.Anchor = System.Windows.Forms.AnchorStyles.Right
+        Me.btnCancel.Location = New System.Drawing.Point(918, 73)
+        Me.btnCancel.Name = "btnCancel"
+        Me.btnCancel.Size = New System.Drawing.Size(75, 29)
+        Me.btnCancel.TabIndex = 11
+        Me.btnCancel.Text = "Cancel"
+        Me.btnCancel.UseVisualStyleBackColor = True
+        '
+        'TableLayoutPanel1
+        '
+        Me.TableLayoutPanel1.ColumnCount = 1
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel1.Controls.Add(Me.Panel3, 0, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.Panel2, 0, 0)
+        Me.TableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TableLayoutPanel1.Location = New System.Drawing.Point(0, 0)
+        Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
+        Me.TableLayoutPanel1.RowCount = 2
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 90.201!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 121.0!))
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(1023, 550)
+        Me.TableLayoutPanel1.TabIndex = 12
+        '
+        'Panel3
+        '
+        Me.Panel3.Controls.Add(Me.radProcess)
+        Me.Panel3.Controls.Add(Me.radSystem)
+        Me.Panel3.Controls.Add(Me.radUser)
+        Me.Panel3.Controls.Add(Me.txtPath)
+        Me.Panel3.Controls.Add(Me.btnCancel)
+        Me.Panel3.Controls.Add(Me.btnBrowse)
+        Me.Panel3.Controls.Add(Me.btnOK)
+        Me.Panel3.Controls.Add(Me.btnExport)
+        Me.Panel3.Controls.Add(Me.btnAdd)
+        Me.Panel3.Controls.Add(Me.btnUpdate)
+        Me.Panel3.Controls.Add(Me.btnDelete)
+        Me.Panel3.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Panel3.Location = New System.Drawing.Point(3, 432)
+        Me.Panel3.Name = "Panel3"
+        Me.Panel3.Size = New System.Drawing.Size(1017, 115)
+        Me.Panel3.TabIndex = 15
+        '
+        'radProcess
+        '
+        Me.radProcess.Anchor = System.Windows.Forms.AnchorStyles.Right
+        Me.radProcess.AutoSize = True
+        Me.radProcess.Location = New System.Drawing.Point(675, 39)
+        Me.radProcess.Name = "radProcess"
+        Me.radProcess.Size = New System.Drawing.Size(70, 16)
+        Me.radProcess.TabIndex = 14
+        Me.radProcess.Text = "Process"
+        Me.radProcess.UseVisualStyleBackColor = True
+        '
+        'radSystem
+        '
+        Me.radSystem.Anchor = System.Windows.Forms.AnchorStyles.Right
+        Me.radSystem.AutoSize = True
+        Me.radSystem.Location = New System.Drawing.Point(573, 39)
+        Me.radSystem.Name = "radSystem"
+        Me.radSystem.Size = New System.Drawing.Size(66, 16)
+        Me.radSystem.TabIndex = 13
+        Me.radSystem.Text = "System"
+        Me.radSystem.UseVisualStyleBackColor = True
+        '
+        'radUser
+        '
+        Me.radUser.Anchor = System.Windows.Forms.AnchorStyles.Right
+        Me.radUser.AutoSize = True
+        Me.radUser.Checked = True
+        Me.radUser.Location = New System.Drawing.Point(471, 39)
+        Me.radUser.Name = "radUser"
+        Me.radUser.Size = New System.Drawing.Size(49, 16)
+        Me.radUser.TabIndex = 12
+        Me.radUser.TabStop = True
+        Me.radUser.Text = "User"
+        Me.radUser.UseVisualStyleBackColor = True
+        '
+        'Panel2
+        '
+        Me.Panel2.Controls.Add(Me.lb)
+        Me.Panel2.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Panel2.Location = New System.Drawing.Point(3, 3)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(1017, 423)
+        Me.Panel2.TabIndex = 14
+        '
+        'MainForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1023, 550)
+        Me.Controls.Add(Me.TableLayoutPanel1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow
+        Me.Name = "MainForm"
+        Me.TableLayoutPanel1.ResumeLayout(False)
+        Me.Panel3.ResumeLayout(False)
+        Me.Panel3.PerformLayout()
+        Me.Panel2.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents fbd As FolderBrowserDialog
+    Friend WithEvents lb As ListBox
+    Friend WithEvents txtPath As TextBox
+    Friend WithEvents btnBrowse As Button
+    Friend WithEvents btnExport As Button
+    Friend WithEvents btnUpdate As Button
+    Friend WithEvents btnDelete As Button
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents btnOK As Button
+    Friend WithEvents btnCancel As Button
+    Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
+    Friend WithEvents Panel3 As Panel
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents radProcess As RadioButton
+    Friend WithEvents radSystem As RadioButton
+    Friend WithEvents radUser As RadioButton
+End Class
---
+++ PathEditor/MainForm.vb
@@ -0,0 +1,119 @@
+﻿Imports System.IO
+Imports System.Security
+Public Class MainForm
+    Sub New()
+        InitializeComponent()
+        GetPathVariable()
+    End Sub
+
+    Private Sub GetPathVariable()
+        Dim envirValue As String = String.Empty
+        Try
+            envirValue = Environment.GetEnvironmentVariable("Path", Target)
+        Catch generatedExceptionName As SecurityException
+            MessageBox.Show(generatedExceptionName.ToString)
+        End Try
+        If envirValue <> "" Then
+            Dim Item As String() = envirValue.Split(New Char() {Path.PathSeparator}, StringSplitOptions.RemoveEmptyEntries)
+            lb.Items.Clear()
+            lb.Items.AddRange(Item)
+            lb.SelectedIndex = 0
+        End If
+    End Sub
+
+
+    Private Sub SetPathVariable()
+        Dim envirValue As String = BuildPathVariable()
+        Try
+            Environment.SetEnvironmentVariable("Path", envirValue, Target)
+        Catch generatedExceptionName As SecurityException
+            MessageBox.Show(generatedExceptionName.ToString)
+        End Try
+    End Sub
+
+    Private Function BuildPathVariable() As String
+        Dim envirValue As String = [String].Empty
+
+        For Each o As [Object] In lb.Items
+            envirValue += o.ToString() + Path.PathSeparator
+        Next
+
+        Return envirValue
+    End Function
+
+    Public ReadOnly Property Target() As EnvironmentVariableTarget
+        Get
+            If radSystem.Checked Then
+                Return EnvironmentVariableTarget.Machine
+            ElseIf radProcess.Checked Then
+                Return EnvironmentVariableTarget.Process
+            Else
+                Return EnvironmentVariableTarget.User
+            End If
+        End Get
+    End Property
+
+#Region "Control Event"
+
+    Private Sub btnCancel_Click(sender As Object, e As EventArgs) Handles btnCancel.Click
+        Close()
+    End Sub
+
+    Private Sub btnBrowse_Click(sender As Object, e As EventArgs) Handles btnBrowse.Click
+        If fbd.ShowDialog() = DialogResult.OK Then
+            txtPath.Text = fbd.SelectedPath
+        End If
+    End Sub
+
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        If Not [String].IsNullOrEmpty(txtPath.Text) Then
+            lb.Items.Add(txtPath.Text)
+        End If
+    End Sub
+
+    Private Sub btnDelete_Click(sender As Object, e As EventArgs) Handles btnDelete.Click
+        Dim idx As Integer = lb.SelectedIndex
+        If idx <> -1 Then
+            lb.Items.RemoveAt(idx)
+            idx -= 1
+            If idx < 0 Then
+                idx = 0
+            End If
+            lb.SelectedIndex = idx
+        End If
+    End Sub
+
+    Private Sub lb_MouseUp(sender As Object, e As MouseEventArgs) Handles lb.MouseUp
+        txtPath.Text = lb.SelectedItem.ToString()
+    End Sub
+
+    Private Sub btnUpdate_Click(sender As Object, e As EventArgs) Handles btnUpdate.Click
+        Dim idx As Integer = lb.SelectedIndex
+        If idx <> -1 Then
+            lb.Items(idx) = txtPath.Text
+        End If
+    End Sub
+    Private Sub btnOK_Click(sender As Object, e As EventArgs) Handles btnOK.Click
+        SetPathVariable()
+        Close()
+    End Sub
+
+    Private Sub CheckedChanged(sender As Object, e As EventArgs) Handles radProcess.CheckedChanged, radSystem.CheckedChanged, radUser.CheckedChanged
+        GetPathVariable()
+        txtPath.Text = lb.SelectedItem.ToString()
+    End Sub
+
+    Private Sub btnExport_Click(sender As Object, e As EventArgs) Handles btnExport.Click
+        Using sfd As New SaveFileDialog()
+            sfd.Filter = "Text files (*.txt)|*.txt"
+            sfd.Title = "Save Environment Variable to Disk"
+            sfd.FileName = "OldPathValue.txt"
+            sfd.DefaultExt = ".txt"
+            If sfd.ShowDialog() = DialogResult.OK Then
+                File.WriteAllText(sfd.FileName, BuildPathVariable())
+            End If
+        End Using
+    End Sub
+#End Region
+
+End Class
